7313e9f427f3cbc042a27abff193411260128e8e
iceRabbit <19802021867@139.com>
2022-01-13 10:22:18 +0800

    init

---
+++ src/main/java/com/icerabbit/nettytango/NettyTangoApplication.java
@@ -0,0 +1,13 @@
+package com.icerabbit.nettytango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyTangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyTangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icerabbit/nettytango/NettyTangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icerabbit.nettytango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyTangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
